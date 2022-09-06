88ecf21ba67eae450317a30045b2a925a72e843b
dimkauzh <106883655+dimkauzh@users.noreply.github.com>
2022-09-06 15:58:26 +0200

    Update- Automatic level switching

--- Level 1.gd
+++ Level 1.gd
@@ -8,7 +8,6 @@ var moving = [Input.is_action_pressed("ui_right"),
 Input.is_action_pressed("ui_left"),
 Input.is_action_pressed("jump")]
 
-
 func _on_Area2D_body_shape_entered(_body_rid, _body, _body_shape_index, _local_shape_index):
 	Global.lose_hearts()
 	Global.hearts_scenes_level()
