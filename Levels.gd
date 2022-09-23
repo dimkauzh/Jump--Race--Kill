070023f4f987e2a491f120208589d1e633c057e8
dimkauzh <uzhdimka@gmail.com>
2022-09-23 23:42:50 +0200

    Bug fixes, ladder repair, testing programm added

--- Levels.gd
+++ Levels.gd
@@ -13,11 +13,14 @@ func _on_ladder_checker_body_entered(body):
 		if body.climbing == false:
 			body.climbing = true
 	
+func _physics_process(delta):
+	on_off_ladder_checker()
 
 func _on_ladder_checker_body_exited(body):
 	if body.is_in_group("Climber"):
 		if body.climbing == true:
 				body.climbing = false
+				off_off_ladder_checker()
 				body.walk_animation()
 
 
@@ -25,3 +28,30 @@ func _on_ladder_checker_body_exited(body):
 func _on_Portal_body_shape_entered(_body_rid, _body, _body_shape_index, _local_shape_index):
 	$Player.position.x = 3528
 	$Player.position.y = -738
+
+func on_off_ladder_checker():
+	if Input.is_action_pressed("ui_down"): 
+		for child in $Off_Ladder_Block.get_children(): child.set_deferred("disabled", true)
+		#get_node("Off_Ladder_Block/CollisionShape2D").set_deferred("disabled", true)
+		#get_node("Off_Ladder_Block/CollisionShape2D2").set_deferred("disabled", true)
+	
+func off_off_ladder_checker():
+	for child in $Off_Ladder_Block.get_children(): child.set_deferred("disabled", false)
+		
+	#$Off_Ladder_Block/CollisionShape2D.set_deferred("disabled", false)
+	#$Off_Ladder_Block/CollisionShape2D2.set_deferred("disabled", false)
+	#$Off_Ladder_Block/CollisionShape2D3.set_deferred("disabled", false)
+	#$Off_Ladder_Block/CollisionShape2D4.set_deferred("disabled", false)
+	#$Off_Ladder_Block/CollisionShape2D5.set_deferred("disabled", false)
+
+#func _on_Area2D_body_entered(body):
+#	if Input.is_action_pressed("ui_down"): $StaticBody2D/CollisionShape2D.set_deferred("disabled", true)
+
+
+
+#func _on_Area2D_body_exited(body):
+#	$StaticBody2D/CollisionShape2D.set_deferred("disabled", false)
+
+
+#func _on_above_ladder_checker_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
+#	$StaticBody2D/CollisionShape2D.set_deferred("disabled", false)
--- Main-Menu.gd
+++ Main-Menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var os = OS.get_name()
+
 func _on_Button_pressed():
 	get_tree().change_scene("res://Levels.tscn")
 
@@ -7,6 +9,10 @@ func _on_Button_pressed():
 func _on_Button2_pressed():
 	get_tree().change_scene("res://howtoplay.tscn")
 
+func _ready():
+	if os == "Windows" or os == "OSX" or os == "Flash" or os == "Haiku" or os == "X11" or os == "WinRT":
+		$exit.visible = true
+		$exit.disabled = false
 
 func _on_exit_pressed():
 	get_tree().quit()
